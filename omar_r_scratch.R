75f84b27d607f9337f4c81b65b23ec0b0a58e6a6
omalik <malik@nceas.ucsb.edu>
2015-02-06 15:10:56 -0800

    Updated
    Added markdown test

--- omar_r_scratch.R
+++ omar_r_scratch.R
@@ -17,6 +17,10 @@ getwd()
 
 ?print
 
+#testing RMarkdown v2:
+H~2~O is a liquid.  2^10^ is 1024.
+
+
 # this is a comment that's been added from a PC. Does it work?
 # this is a response from the cloud. Yes, it works!
 # cool! 
